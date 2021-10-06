623483be6208e35bd131b1e81dfbd1e90da392d2
lostship <418181589@qq.com>
2021-10-06 11:02:30 +0800

    generic

--- src/main/java/com/example/corejava/generic/Pair.java
+++ src/main/java/com/example/corejava/generic/Pair.java
@@ -2,6 +2,7 @@ package com.example.corejava.generic;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 import com.example.corejava.classes.class1.Manager;
@@ -40,14 +41,18 @@ public class Pair<T> {
 
     /**
      * 子类通配符
+     * 
+     * 选用哪种通配符可以参考PECS（producer-extends, consumer-super）原则。
+     * 比如此处的list是生产者，因此是List<? extends T>。
+     * 通常遇到Comparable和Comparator就是消费者，消费T实例，所以经常遇到Comparable<? super T>。
+     * 
+     * 如果既是生产者，又是消费者，可以使用泛型方法。
      */
-    public static void print1(ArrayList<? extends Exception> list) {
+    public static void print1(List<? extends Exception> list) {
         // 调用获取返回值的方法时，可以使用更宽松的类型接收
-        if (!list.isEmpty()) {
-            Exception e = list.get(0);
-            Throwable e1 = list.get(0);
-            Object e2 = list.get(0);
-        }
+        Exception e = list.get(0);
+        Throwable e1 = list.get(0);
+        Object e2 = list.get(0);
 
         // 调用传递值的方法时，子类限定通配符对修改关闭，拒绝null以外的任何参数
         list.add(null);
@@ -60,13 +65,36 @@ public class Pair<T> {
     }
 
     /**
+     * 泛型方法
+     * 
+     * 类型参数和通配符之间具有双重性，很多方法用两种方式声明都可以达到目的。
+     * 但是他们之间也有细微差别。使用时根据可读性和可实现功能需求灵活选择。
+     */
+    public static <E extends Exception> E print1(List<E> list, E oneElement) {
+        // 调用获取返回值的方法时，可以使用更宽松的类型接收
+        Exception e = list.get(0);
+        Throwable e1 = list.get(0);
+        Object e2 = list.get(0);
+
+        // 调用传递值的方法时，子类限定类型参数拒绝实际参数类型和null以外的任何参数
+        list.add(null);
+        // list.add(new Exception()); // error
+        // list.add(new IOException()); // error
+        // list.add(new Throwable()); // error
+        // list.add(new Object()); // error
+        // list.add(new String()); // error
+        list.add(list.get(0));
+        list.add(oneElement);
+
+        return oneElement;
+    }
+
+    /**
      * 超类通配符
      */
-    public static void print2(ArrayList<? super Exception> list) {
+    public static void print2(List<? super Exception> list) {
         // 调用获取返回值的方法时，只能使用Object类型接收
-        if (!list.isEmpty()) {
-            Object e = list.get(0);
-        }
+        Object e = list.get(0);
 
         // 调用传递值的方法时，接收泛型类或子类的参数，拒绝接收更宽松的类型或其它类型
         list.add(null);
@@ -78,11 +106,9 @@ public class Pair<T> {
         // list.add(list.get(0)); // error
     }
 
-    public static void print3(ArrayList<Object> list) {
+    public static void print3(List<Object> list) {
         // 调用获取返回值的方法时，只能使用Object类型接收
-        if (!list.isEmpty()) {
-            Object e = list.get(0);
-        }
+        Object e = list.get(0);
 
         // 调用传递值的方法时，接收任何类型参数
         list.add(null);
@@ -91,19 +117,15 @@ public class Pair<T> {
         list.add(new Throwable());
         list.add(new Object());
         list.add(new String());
-        if (!list.isEmpty()) {
-            list.add(list.get(0));
-        }
+        list.add(list.get(0));
     }
 
     /**
      * 无限定通配符
      */
-    public static void print4(ArrayList<?> list) {
+    public static void print4(List<?> list) {
         // 调用获取返回值的方法时，只能使用Object类型接收
-        if (!list.isEmpty()) {
-            Object e = list.get(0);
-        }
+        Object e = list.get(0);
 
         // 调用传递值的方法时，无限定通配符对修改关闭，拒绝null以外的任何参数
         list.add(null);
@@ -125,12 +147,12 @@ public class Pair<T> {
         // print2(new ArrayList<IOException>()); // error
 
         // subtyping 子类型规则
-        Predicate<Pair> p1 = obj -> obj.hashCode() % 2 != 0;
+        Predicate<Pair<?>> p1 = obj -> obj.hashCode() % 2 != 0;
         Predicate<Object> p2 = null;
 
-        Predicate<? extends Pair> p3 = p1;
+        Predicate<? extends Pair<?>> p3 = p1;
 
-        Predicate<? super Pair> p4 = p1;
+        Predicate<? super Pair<?>> p4 = p1;
         p4 = p2;
 
         Predicate<?> p5 = p1;
@@ -170,15 +192,11 @@ public class Pair<T> {
     }
 
     /**
-     * 通配符捕获
+     * 这个方法签名很复杂，重视可读性的话对外可以提供一个public void swap(Pair<?> p)的方法，内部利用更加复杂的泛型方法。
      */
-    public static <T> void swapHelper(Pair<T> p) {
+    private static <T> void swapHelper(Pair<T> p) {
         T t = p.first;
         p.first = p.second;
         p.second = t;
-    }
-
-    public static void removeIf(Predicate<Pair> p) {
-
     }
 }
