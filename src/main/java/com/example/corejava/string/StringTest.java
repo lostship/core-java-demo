a0ec1fd19f5bc5ca11bd260b9bb38419382eb9bc
lostship <418181589@qq.com>
2021-10-08 11:00:30 +0800

    string

---
+++ src/main/java/com/example/corejava/string/StringTest.java
@@ -0,0 +1,25 @@
+package com.example.corejava.string;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String a = "1";
+
+        String b = "" + 1;
+
+        int n = 1;
+        String c = "" + n;
+
+        final int m = 1;
+        String d = "" + m;
+
+        String e = Integer.toString(1);
+
+        String f = new String("1");
+
+        System.out.println(a == b);
+        System.out.println(a == c);
+        System.out.println(a == d);
+        System.out.println(a == e);
+        System.out.println(a == f);
+    }
+}
